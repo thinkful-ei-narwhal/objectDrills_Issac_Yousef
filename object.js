c4d213bb7cebe6d11c75dafb7a9f8a1b4baf9558
Isaac Elesia <isaacelesia@gmail.com>
2020-03-17 14:03:30 -0500

    added use strict

--- object.js
+++ object.js
@@ -1,18 +1,19 @@
-function createMyObject () {
-  const obj = {
-    foo: 'bar',
-    answerToUniverse: 42,
-    'olly olly': 'oxen free',
-    sayHello: function () {
-      return 'hello'
-    }
-  }
-  return obj
+'use strict';
+function createMyObject() {
+	const obj = {
+		foo: 'bar',
+		answerToUniverse: 42,
+		'olly olly': 'oxen free',
+		sayHello: function() {
+			return 'hello';
+		}
+	};
+	return obj;
 }
 
-function updateObject (obj) {
-  obj.foo = 'foo'
-  obj.bar = 'bar'
-  obj.bizz = 'bizz'
-  obj.bang = 'bang'
+function updateObject(obj) {
+	obj.foo = 'foo';
+	obj.bar = 'bar';
+	obj.bizz = 'bizz';
+	obj.bang = 'bang';
 }
