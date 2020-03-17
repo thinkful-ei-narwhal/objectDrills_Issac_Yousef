2df3c5e7e73fb33d8d0ff25ab43abc55901fea09
Yghaddar <58191171+yghaddar@users.noreply.github.com>
2020-03-17 14:54:02 -0400

    added create and update

---
+++ object.js
@@ -0,0 +1,18 @@
+function createMyObject () {
+  const obj = {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function () {
+      return 'hello'
+    }
+  }
+  return obj
+}
+
+function updateObject (obj) {
+  obj.foo = 'foo'
+  obj.bar = 'bar'
+  obj.bizz = 'bizz'
+  obj.bang = 'bang'
+}
